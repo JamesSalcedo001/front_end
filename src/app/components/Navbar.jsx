07178c312ffc64b375fb249264d0ef34017521c8
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2025-01-15 02:03:31 -0600

    Center items and add some padding and shadow to navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { FaInstagram } from "react-icons/fa";
 
 export default function Navbar() {
     return (
-        <nav className="fixed top-0 left-0 right-0 bg-gray-100">
+        <nav className="fixed top-0 left-0 right-0 bg-gray-100 flex items-center justify-between p-4 shadow-md">
             <h1 className="text-5xl font-bold text-green-700">RTO</h1>
             <ul>
                 <li>
@@ -22,7 +22,7 @@ export default function Navbar() {
                 href="https://instagram.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-pink-500"
+                className="text-pink-500 text-2xl"
             >
                 <FaInstagram />
             </a>
