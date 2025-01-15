8538ec75bf65ff8fb241fc1300619f0ba4bcfbcd
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2025-01-15 01:06:55 -0600

    Add components directory, add sections in home component for home, tribute, about and footer, minimal styling for layout

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,31 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main>
+      <section id="home" className="min-h-screen pt-40 bg-gray-100">
+        <div className="flex items-center justify-center h-full">
+          <h1>Carousel Section</h1>
+        </div>
+      </section>
+
+      <section id="tribute" className="min-h-screen bg-white">
+        <div className="flex items-center justify-center h-full">
+          <h1>Tribute Section</h1>
+        </div>
+      </section>
+
+      <section id="about" className="min-h-screen bg-gray-100">
+        <div className="flex items-center justify-center h-full">
+          <h1>About Section</h1>
+        </div>
+      </section>
+
+      <section id="footer" className="bg-gray-200 py-20">
+        <div>
+          <h1>Footer Section</h1>
+        </div>
+      </section>
+
+    </main>
   );
 }
